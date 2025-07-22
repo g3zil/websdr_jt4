79e787510226c8e4032ef5db9b33557e715ed707
Gwyn Griffiths <gwyn@autonomousanalytics.com>
2025-07-22 12:02:39 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,14 +4,6 @@
 ### Also installs command line and bash modules 
 ### V1.2 for Raspberry Pi and Ubuntu 24.04
 
-########################################################
-# Only user-set installation variables are in this block
-########################################################
-# set up receiver details to go into database table via python
-RX_GRID=JO01qj           #  Maidenhead for Margate WebSDR, not where browser is located!              
-RX_ID=TRIG01/G3ZIL       # The /G3ZIL is a suggested addition, use your callsign to show who is using WebSDR
-########################################################
-
 # No need for user to edit lines below
 #######################################################
 # check that required dependencies are met, except for browser and WSJT-X assumed available and connected by virtual cable
