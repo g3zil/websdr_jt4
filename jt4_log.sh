994241d96381f4fb5fdc3ac7726822f31cf89594
Gwyn Griffiths <gwyn@autonomousanalytics.com>
2026-01-15 13:46:09 +0000

    Update jt4_log.sh

--- jt4_log.sh
+++ jt4_log.sh
@@ -8,6 +8,9 @@
 # set up directory and file where latest decode and non-decode lines will be found
 #
 BASE_DIR=$(pwd)
+
+source .venv/bin/activate  # run all the code in the protected environment readme now has note this is a must
+
 source ${BASE_DIR}/jt4_config.ini    # the config.ini file holds the callsign and grid of the receiver, other metadata got from jt4 decodes
 
 JT4_DATA_FILE="$(echo ${BASE_DIR} | cut -d/ -f-3)/.local/share/WSJT-X/ALL.TXT"   #  This is where wsjtx puts the data
@@ -30,9 +33,9 @@ then
     tail -1 ${JT4_DATA_FILE} | sed 's/,[ \t]\?/,/g' | tr '_' ' ' >${BASE_DIR}/temp.txt                            # get last line from the ALL.TXT, sed gets rid of white space, tr changes _ to space
     DECODE_CAPTURE_DATE=$( date -u --date="1 minute ago" +%Y-%m-%dT%H:%M:00Z)                                     # easier to get time in required format this way
     awk 'FS = " ", OFS="," {print $1,$2,$3,$4,$5,$6,$7,$8,$9,$10}' <${BASE_DIR}/temp.txt >${BASE_DIR}/temp1.csv   # reformat to a csv
-    /usr/bin/python3 ${BASE_DIR}/azi_calc.py ${DECODE_CAPTURE_DATE} ${RX_GRID} ${RX_ID}                           # calculate lat/lon/azimuths/distance
+    python3 ${BASE_DIR}/azi_calc.py ${DECODE_CAPTURE_DATE} ${RX_GRID} ${RX_ID}                           # calculate lat/lon/azimuths/distance
     echo "Decode data and added variables written to spots_azi.csv file"
-    ~/websdr_jt4/.venv/bin/python3  ${BASE_DIR}/jt4_upload.py ${BASE_DIR}/spots_azi.csv                                         # upload to table jt4 database tutorial on wd1 server
+    python3  ${BASE_DIR}/jt4_upload.py ${BASE_DIR}/spots_azi.csv                                         # upload to table jt4 database tutorial on wd1 server
     tail -1 ${JT4_DATA_FILE} > ${BASE_DIR}/previous_upload.txt                                                    # this is one we just uploaded, not to be duped
 
   #  This code runs a JT4 detector,a correlator that should provide a detection, not a decode, at lower SNR - perhaps!
@@ -40,7 +43,7 @@ then
     WAV_FILE=$(ls -ltr ${BASE_DIR}/save| tail -n 1 | awk '{print $9}')              # get the JT4 even minute wav file name to process 
     echo "Detection program processing file "${WAV_FILE}
     sox ${BASE_DIR}/save/${WAV_FILE} -r 11025 ${BASE_DIR}/11025.wav                 # resample to 11025 sps (now this is 1/4 of 44100 sps!)
-    ~/websdr_jt4/.venv/bin/python3 ${BASE_DIR}/jt4_detect.py ${DECODE_CAPTURE_DATE} ${BASE_DIR}/11025.wav     # do the processing!
+    python3 ${BASE_DIR}/jt4_detect.py ${DECODE_CAPTURE_DATE} ${BASE_DIR}/11025.wav     # do the processing!
   fi
 else
 
@@ -57,7 +60,7 @@ else
        then
          echo 'Correct odd minute - so process signal and noise'
          ${BASE_DIR}/sn_calc.sh ${WAV_FILE}                                        # script uses SOX to calculate signal level and noise
-         /usr/bin/python3 ${BASE_DIR}/sn_upload.py ${BASE_DIR}/signal_noise.csv    # uploads to jt4 table on wd1 with mode ident CW together with metadata
+         python3 ${BASE_DIR}/sn_upload.py ${BASE_DIR}/signal_noise.csv    # uploads to jt4 table on wd1 with mode ident CW together with metadata
        else
          echo 'File time mismatch: WSJT-X likely not running'
        fi
